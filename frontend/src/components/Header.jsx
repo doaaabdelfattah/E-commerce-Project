2fa47eb55cdcd2af763e052b5d0adc6c307b4bdc
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-01 05:38:07 +0300

    [upload] repo + Setup + Add main Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MdEmail } from "react-icons/md";
+import { FaPhone, FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-white">
+      <div className="header-top bg-[#FAF9F7] md-lg:hidden ">
+        <div className="w-[85%] lg:w[90%] mx-auto">
+          <div className="flex w-full justify-between items-center h-[60px] text-[#0F233C]">
+            {/* =========== Left side ========= */}
+            <ul className="flex justify-start items-center gap-8 font-semibold">
+              <li className="flex relative justify-center items-center gap-2 after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] hover:text-[#BC9B80]">
+                <span>
+                  <MdEmail />
+                </span>
+                <span>support@gmail.com</span>
+              </li>
+              <li className="flex relative justify-center items-center gap-2 after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] hover:text-[#BC9B80]">
+                <span>
+                  <FaPhone />
+                </span>
+                <span>+1234545667</span>
+              </li>
+            </ul>
+            {/* =========== right side ========= */}
+            <div className="relative">
+              <div className="flex justify-center items-center gap-10 ">
+                {/* - - Social Links - - */}
+                <div className="flex justify-center items-center gap-4 text-[1.5rem]">
+                  <a className="hover:text-[#BC9B80]" href="#">
+                    <FaFacebook />
+                  </a>
+                  <a className="hover:text-[#BC9B80]" href="#">
+                    <FaTwitter />
+                  </a>
+                  <a className="hover:text-[#BC9B80] " href="#">
+                    <FaLinkedin />
+                  </a>
+                </div>
+                {/* - -   - - */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
