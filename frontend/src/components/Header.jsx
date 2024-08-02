c8b0c1f73364503c47914a6b63ecc3d0c5bc5052
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-03 02:00:40 +0300

    [update] main header and adjust css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail } from "react-icons/md";
 import {
   FaPhone,
@@ -7,17 +7,23 @@ import {
   FaLinkedin,
   FaUser,
   FaLock,
+  FaRegHeart,
 } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Header = () => {
-  const user = false;
+  const { pathname } = useLocation();
+  const [showSideBar, setshowSideBar] = useState(true);
+  const user = true;
+  const wishlist = 4;
   return (
     <div className="w-full bg-white">
       <div className="header-top bg-[#FAF9F7] md-lg:hidden ">
         <div className="w-[85%] lg:w[90%] mx-auto">
           <div className="flex w-full justify-between items-center h-[60px] text-[#0F233C]">
-            {/* =========== Left side ========= */}
+            {/* =========== topHeader - Left side ========= */}
             <ul className="flex justify-start items-center gap-8 font-semibold">
               <li className="flex relative justify-center items-center gap-2 after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] hover:text-[#BC9B80]">
                 <span>
@@ -32,7 +38,7 @@ const Header = () => {
                 <span>+1234545667</span>
               </li>
             </ul>
-            {/* =========== right side ========= */}
+            {/* =========== topHeader - right side ========= */}
             <div className="relative">
               <div className="flex justify-center items-center gap-10 ">
                 {/* - - Social Links - - */}
@@ -72,6 +78,249 @@ const Header = () => {
                     <span>Login </span>
                   </Link>
                 )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*  ============== Main HEADER ======== */}
+
+      <div className="w-white mt-6 pt-4">
+        <div className="w-[85%] h-[100px] lg:w[90%] mx-auto">
+          <div className=" md-lg:h[100px] flex justify-between items-center flex-wrap">
+            {/* ===== Left part ====== */}
+            <div className="md-lg:w-full w-3/12 md-lg:pt-4">
+              <div className="flex justify-between items-center">
+                <Link to="/">
+                  <img
+                    src="http://localhost:3000/images/logo-only.png"
+                    alt=" logo"
+                    style={{ width: "220px" }}
+                  />
+                </Link>
+
+                <div
+                  className="justify-center items-center cursor-pointer lg:hidden xl:hidden md-lg:flex hidden"
+                  onClick={() => setshowSideBar(!showSideBar)}
+                >
+                  <span>
+                    <GiHamburgerMenu size="40px" color="#4B505E" />
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* ====== two-third part ====== */}
+            <div className="w-9/12 md-lg:w-full">
+              <div className="flex justify-between items-center md-lg:justify-center flex-wrap pl-8">
+                {/* - - Menu - - */}
+                <ul className="flex justify-start items-start gap-8 uppercase md-lg:hidden text-lg ">
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/" ? "text-[#BC9B80]" : "text-slate-600"
+                      } hover:text-[#BC9B80]`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/shop"
+                          ? "text-[#BC9B80]"
+                          : "text-slate-600"
+                      } hover:text-[#BC9B80]`}
+                    >
+                      Shop
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/blog"
+                          ? "text-[#BC9B80]"
+                          : "text-slate-600"
+                      } hover:text-[#BC9B80]`}
+                    >
+                      Blog
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/about"
+                          ? "text-[#BC9B80]"
+                          : "text-slate-600"
+                      } hover:text-[#BC9B80]`}
+                    >
+                      about us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/contact"
+                          ? "text-[#BC9B80]"
+                          : "text-slate-600"
+                      } hover:text-[#BC9B80]`}
+                    >
+                      contact us
+                    </Link>
+                  </li>
+                </ul>
+                {/* - - Icons - - */}
+                <div className="flex md-lg:hidden justify-center items-center gap-5">
+                  <div className="flex justify-center gap-5">
+                    {/* Wishlist */}
+                    <div className="flex relative justify-center items-center cursor-pointer rounded-full w-[40px] h-[40px]  text-[#1F212A] hover:text-[#BC9B80]">
+                      <span className="text-3xl">
+                        <FaRegHeart />
+                      </span>
+                      <div className="absolute w-[20px] h-[20px] bg-[#BC9B80] text-white flex justify-center items-center rounded-full -top-[3px] -right-[6px]">
+                        {wishlist}
+                      </div>
+                    </div>
+                    {/* Shopping Cart */}
+                    <div className="flex relative justify-center items-center cursor-pointer rounded-full w-[40px] h-[40px] text-[#1F212A] hover:text-[#BC9B80]">
+                      <span className="text-3xl">
+                        <FiShoppingCart />
+                      </span>
+                      <div className="absolute w-[20px] h-[20px] bg-[#BC9B80] text-white flex justify-center items-center rounded-full -top-[3px] -right-[6px]">
+                        {wishlist}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* ========= Menu when press burger icon =========== */}
+      <div className="hidden md-lg:block">
+        <div
+          onClick={() => setshowSideBar(true)}
+          className={`fixed duration-300 transition-all ${
+            showSideBar ? "invisible" : "visible"
+          } w-screen h-screen md-lg:block  bg-[rgba(188,155,128,0.55)] mt-5 top-0 left-0 z-20`}
+        ></div>
+        <div
+          className={`w-[300px] z-[999] transition-all duration-300 fixed ${
+            showSideBar
+              ? "-left-[300px]"
+              : "left-0 top-0 overflow-auto bg-white h-screen py-6 px-8 "
+          }`}
+        >
+          <Link to="/">
+            <img
+              src="http://localhost:3000/images/logo-sm.png"
+              alt=" logo"
+              style={{ width: "220px", marginTop: "20px" }}
+            />
+          </Link>
+          {/* - - Menu - - */}
+          <ul className="m-4 pt-3 flex flex-col h-[60%] justify-start items-start gap-5 uppercase text-lg ">
+            <li>
+              <Link
+                className={`py-2 block ${
+                  pathname === "/" ? "text-[#BC9B80]" : "text-slate-600"
+                } hover:text-[#BC9B80]`}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`py-2 block ${
+                  pathname === "/shop" ? "text-[#BC9B80]" : "text-slate-600"
+                } hover:text-[#BC9B80]`}
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`py-2 block ${
+                  pathname === "/blog" ? "text-[#BC9B80]" : "text-slate-600"
+                } hover:text-[#BC9B80]`}
+              >
+                Blog
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                className={`py-2 block ${
+                  pathname === "/about" ? "text-[#BC9B80]" : "text-slate-600"
+                } hover:text-[#BC9B80]`}
+              >
+                about us
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`py-2 block ${
+                  pathname === "/contact" ? "text-[#BC9B80]" : "text-slate-600"
+                } hover:text-[#BC9B80]`}
+              >
+                contact us
+              </Link>
+            </li>
+          </ul>
+          {/* - - Social Links - - */}
+          <div className="flex justify-center items-center gap-4 text-[1.5rem]">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="hover:text-[#BC9B80]" href="#">
+              <FaFacebook />
+            </a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="hover:text-[#BC9B80]" href="#">
+              <FaTwitter />
+            </a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="hover:text-[#BC9B80] " href="#">
+              <FaLinkedin />
+            </a>
+          </div>
+          {/* User */}
+          <div className="m-4 border-t-2 pt-3">
+            {user ? (
+              <Link
+                className="flex cursor-pointer justify-center items-center gap-2"
+                to="/dashboard"
+              >
+                <span>
+                  <FaUser />
+                </span>
+                <span> Doaa Abdelfattah </span>
+              </Link>
+            ) : (
+              <Link
+                className="flex cursor-pointer justify-center items-center gap-2"
+                to="/login"
+              >
+                <span>
+                  <FaLock />
+                </span>
+                <span>Login </span>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* =============== Second Line ============= */}
+
+      <div className="w-[85%] border-t-2 lg:w-[90%] mx-auto">
+        <div className="flex w-full flex-wrap md-lg:gap-8">
+          {/* Part ONE I */}
+          <div className="w-3/12 md-lg:w-full">
+            <div className="bg-white relative">
+              <div className="h-[50px] mt-5 rounded-md  flex justify-center cursor-pointer transition-all duration-300 custom-before pt-2">
+                <div></div>
               </div>
             </div>
           </div>
