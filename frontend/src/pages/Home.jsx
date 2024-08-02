d5a5fd654805cc754d7d01a9ee28ddace03233f2
nadashaban10 <nadashaban10@github.com>
2024-08-02 03:00:08 +0300

    1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Category from "../components/Category";
+import ProductFeatures from "../components/product/ProductFeatures";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
+      <Category />
+      <div className="py-[50px]">
+      <ProductFeatures />
+      </div>
     </div>
   );
 };
