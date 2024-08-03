459c60c5deeea577ad5b65853bb3cf9b7f4c364e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-03 12:37:14 +0300

    [update - Front] Fix banner section

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,54 +1,57 @@
 import React from "react";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 1
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    return (
-        <div className="w-full md:mt-6 lg:mt-6">
-            <div className="w-[85%] lg:w-[90%] mx-auto">
-                <div className="w-full flex flex-wrap md:gap-8 lg:gap-8">
-                    <div className="w-full">
-                        <div className="my-8">
-                            <Carousel
-                                autoPlay={true}
-                                infinite={true}
-                                arrows={true}
-                                showDots={true}
-                                responsive={responsive}>
-                                {
-                                    [1, 2, 3, 4, 5, 6].map((img, index) => (
-                                        <Link key={index} to="#">
-                                            <img src={`http://localhost:3000/images/banner/${img}.jpg`} alt="banner" />
-                                        </Link>
-                                    ))
-                                }
-                            </Carousel>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="w-full md:mt-6 lg:mt-6">
+      <div className="w-full lg:w-[90%] mx-auto">
+        <div className="w-full flex flex-wrap md:gap-8 lg:gap-8">
+          <div className="w-full">
+            <div className="my-8 ">
+              <Carousel
+                autoPlay={true}
+                infinite={true}
+                arrows={true}
+                showDots={true}
+                responsive={responsive}
+              >
+                {[1, 2, 3, 4, 5].map((img, index) => (
+                  <Link key={index} to="#">
+                    <img
+                      className="banner-img"
+                      src={`http://localhost:3000/images/banner/${img}.jpg`}
+                      alt="banner"
+                    />
+                  </Link>
+                ))}
+              </Carousel>
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
