9ed50a05e82e010349a673aaa7a50f4ed1a9b77e
nadashaban10 <nadashaban10@github.com>
2024-08-03 13:47:33 +0300

    top features setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Category from "../components/Category";
 import ProductFeatures from "../components/product/ProductFeatures";
-
+import Products from "../components/Product"
 const Home = () => {
   return (
     <div>
@@ -12,6 +12,24 @@ const Home = () => {
       <Category />
       <div className="py-[50px]">
       <ProductFeatures />
+      </div>
+      <div className="py-10">
+        <div className="w-[85%] flex flex-wrap mx-auto"  >
+          <div className="grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7" >
+            <div className="overflow-hidden" >
+              <Products />
+            </div>
+            <div className="overflow-hidden" >
+              <Products />
+            </div>
+            <div className="overflow-hidden" >
+              <Products />
+            </div>
+            
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
