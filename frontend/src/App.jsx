d8e0953de5f5678c1517894f97f360a978f9f639
nadashaban10 <nadashaban10@github.com>
2024-08-05 03:45:06 +0300

    shipping page design done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
 import Cart from "./pages/Cart";
-
 import Shop from "./pages/Shop";
+import Shipping from "./pages/Shipping";
 
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Cart" element={<Cart />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/shipping" element={<Shipping />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,19 +4,18 @@ import { FaFacebookF} from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { useSelector } from 'react-redux';
-import { FaHeart } from "react-icons/fa6";
-import { FaCartShopping } from "react-icons/fa6";
+import { Element } from 'react-scroll';
 
 const Footer = () => {
 
 
     return (
+        < Element name="footer">
         <footer className='bg-[#f3f6fa]'>
             <div className='w-[85%] flex flex-wrap mx-auto border-b py-16 md-lg:pb-10 sm:pb-6'>
                 <div className='w-3/12 lg:w-4/12 sm:w-full'>
                     <div className='flex flex-col gap-3'>
-                        <img className='w-[190px] h-[70px]' src="http://localhost:3000/images/logo.png" alt="logo" />
+                        <img className='w-[190px] h-[70px]' src="http://localhost:3000/images/logo-only.png" alt="logo" />
                         <ul className='flex flex-col gap-2 text-slate-600'>
                             <li>Address :  2504 Ivins Avenue, Egg Harbor Township, NJ 08234,</li>
                             <li>Phone : 4343434344</li>
@@ -110,6 +109,7 @@ const Footer = () => {
 
            
         </footer>
+        </Element>
     );
 };
 
