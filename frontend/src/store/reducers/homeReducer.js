0d7c3c19463f63dab831e05fdecbb10305f37a7d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-07 12:33:05 +0300

    [Setup] initial setup redux package toolkit

---
+++ frontend/src/store/reducers/homeReducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { homeReducer } from './index';
+
+
+
+export const homeReducer = createSlice({
+  name: 'home',
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+
+  }
+})
+
+export default function homeReducer.reducer
+
