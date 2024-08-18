a6623d295b8e8a6559e9698473a9b04f5be3410b
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-18 03:49:40 +0300

    fix all frontend errors + back to fakestore api

---
+++ frontend/website/src/redux/reducers/sortedProductSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+// Initial state 
+const initialState = {
+  sortedProductsArray: [],
+};
+
+export const fetchSortedProducts = createAsyncThunk('sortedProducts/fetchSortedProducts', async () => {
+  try {
+    const response = await api.get('products');
+    // console.log('API response:', response.data); // Log API response
+    return response.data;
+  } catch (error) {
+    // console.error('API error:', error.message); // Log any errors
+    return error.message;
+  }
+});
+
+// Slice
+const sortedProductsSlice = createSlice({
+  name: 'sortedProducts',
+  initialState,
+  reducers: {
+    sortProductsByPriceAsc: (state) => {
+      state.sortedProductsArray.sort((a, b) => a.price - b.price);
+    },
+    sortProductsByPriceDesc: (state) => {
+      state.sortedProductsArray.sort((a, b) => b.price - a.price);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSortedProducts.fulfilled, (state, action) => {
+        // console.log('Action payload:', action.payload); // Log action payload
+        state.sortedProductsArray = action.payload;
+      });
+  },
+});
+
+export const { sortProductsByPriceAsc, sortProductsByPriceDesc } = sortedProductsSlice.actions;
+
+export default sortedProductsSlice.reducer;
--- frontend/website/src/redux/store.js
+++ frontend/website/src/redux/store.js
@@ -1,23 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./reducers/categoriesSlice";
 import productsReducer from "./reducers/productsSlice";
-<<<<<<< HEAD:frontend/src/redux/store.js
 import sortedProductReducer from "./reducers/sortedProductSlice";
-=======
 // import cartReducer from "./reducers/cartSlice";
 
->>>>>>> 63f7f4913124357beff2febd5ff6f6e1fac775a7:frontend/website/src/redux/store.js
 
 export const store = configureStore({
-    reducer: {
-        categories: categoriesReducer,
-        products: productsReducer,
-<<<<<<< HEAD:frontend/src/redux/store.js
-        sortedProducts: sortedProductReducer,
-=======
-        // cart: cartReducer,
+        reducer: {
+                categories: categoriesReducer,
+                products: productsReducer,
+                sortedProducts: sortedProductReducer,
+                // cart: cartReducer,
 
-
->>>>>>> 63f7f4913124357beff2febd5ff6f6e1fac775a7:frontend/website/src/redux/store.js
-    }
+        }
 });
