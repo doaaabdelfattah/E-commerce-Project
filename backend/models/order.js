2aae110129b3239a963645fa8e6d2e96da1d1486
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-08-26 15:19:54 +0300

    26/8/2024

--- backend/models/order.js
+++ backend/models/order.js
@@ -8,7 +8,7 @@ const orderSchema = new mongoose.Schema({
   zip: { type: String, required: true },
   country: { type: String, required: true },
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   phone: { type: String, required: true },
   status: { type: String, default: 'Pending', required: true },
   totalPrice: { type: Number },
