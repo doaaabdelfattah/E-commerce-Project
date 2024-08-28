5dcd4a6f25a0bac307a9e5bc6fc9497610ac9224
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-08-28 04:59:49 +0300

    Final fix to home page

--- frontend/website/src/components/Header.jsx
+++ frontend/website/src/components/Header.jsx
@@ -54,7 +54,6 @@ const Header = () => {
     navigate("/Login");
   };
 
- 
   // ========================== Cart
   const cart = useSelector((state) => state.cart.items);
   const totalItems = cart.length;
@@ -366,59 +365,6 @@ const Header = () => {
           </div>
         </div>
       </div>
-
-      {/* =============== Second Line Main HEADER ============= */}
-<<<<<<< HEAD
-
-      <div className="w-[85%] border-t-[1.5px] lg:w-[90%] mx-auto">
-        <div className="flex w-full flex-wrap md-lg:gap-4">
-          {/* Part ONE I All Categories Button */}
-          <div className="w-3/12 md-lg:w-full">
-            <div className="bg-white relative">
-              {/* Category Button */}
-              <div
-                onClick={() => setshowCategory(!showCategory)}
-                className=" duration-500 transition-all text-[#1F212A] h-[60px] mt-5 flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-semibold cursor-pointer bg-[#FAF9F7] custom-before pt-2"
-              >
-                <div className="flex justify-center items-center gap-3">
-                  <span className="text-2xl"></span>
-                  <span>All categories</span>
-                </div>
-                <span className="text-2xl">
-                  <MdKeyboardArrowDown />
-                </span>
-              </div>
-
-              {/* Category list */}
-              <div
-                className={`${
-                  showCategory ? "h-[0]" : "h-fit"
-                } overflow-hidden transition-all md-lg:relative duration-500 absolute z-[20] w-full bg-[#FAF9F7]`}
-              >
-                <ul className="py-2 m-[10px] font-medium">
-                  {categories.map((category) => (
-                    
-                    <li
-                      key={category.id}
-                      className="flex justify-start m-[10px] relative hover:translate-x-3 transition-all duration-500 gap-2 items-center py-[6px] px-[24px]"
-                    >
-                      <Link
-                        className="block"
-                        onClick={() => handleCategoryClick(category.id)}
-                      >
-                        {category.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <SearchBar />
-        </div>
-      </div>
-
     </div>
   );
 };
